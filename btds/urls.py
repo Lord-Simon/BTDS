2796cbb2e541120e5b6575f448cdc1bc1418e21b
Michael WIlliams <draringi@draringi.net>
2013-03-18 20:38:36 -0300

    Update urls.py
    sid -> bid
    Bloody Book ID!!!

--- btds/urls.py
+++ btds/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns("btds",
     url(r'^$', 'views.index', name='index'),
     url(r'^(?P<sid>\d+)/$', 'views.series', name='series'),
-    url(r'^\d+/(?P<sid>\d+)/$', 'views.volume', name='volume'),
+    url(r'^\d+/(?P<bid>\d+)/$', 'views.volume', name='volume'),
 )
