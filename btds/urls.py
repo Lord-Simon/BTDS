7a4f0c967005058a72d65fc0fc17c67f2a538a4d
Michael WIlliams <draringi@draringi.net>
2013-03-18 21:17:16 -0300

    Update urls.py
    /b/ to get to book.
    (don't have the two conflict)

--- btds/urls.py
+++ btds/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns("btds.views",
     url(r'^$', 'index', name='index'),
     url(r'^(?P<sid>\d+)/$', 'series', name='series'),
-    url(r'^(?P<vid>\d+)/$', 'volume', name='volume'),
+    url(r'^/b/(?P<vid>\d+)/$', 'volume', name='volume'),
 )
