4ce046bb7489963999bf13d62e9fb7f4cd81ddb6
Simon Walter <semen.walter@gmail.com>
2013-03-26 02:25:30 +0100

    Login and some other strange things...

--- btds/urls.py
+++ btds/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns("btds.views",
     url(r'^book/(?P<vid>\d+)/$', 'volume', name='btds_volume'),
 )
 urlpatterns += patterns("",
-    (r'^feed/$', VolumeFeed()),
-    (r'^feedl/$', LinkFeed()),
+    url(r'^login/$','django.contrib.auth.views.login', {'template_name': 'btdst/login.html'}, name="login"),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '../'}, name="logout"),
+    url(r'^feed/$', VolumeFeed()),
+    url(r'^feedl/$', LinkFeed()),
 )
--- btds/views.py
+++ btds/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from btds.models import *
 from django.conf import settings
+from django.template import RequestContext
 
 def index(request):
     novels = Novel.objects.all()
-    return render_to_response('btdst/index.html',{'novels':novels})
+    return render_to_response('btdst/index.html', {'novels':novels}, context_instance=RequestContext(request))
 
 def series(request, sid):
     novel = get_object_or_404(Novel, id = sid)
     series = novel.volume_set.all()
-    return render_to_response('btdst/series.html',{'series':series})
+    return render_to_response('btdst/series.html',{'series':series}, context_instance=RequestContext(request))
 
 def volume(request, vid):
     volume = get_object_or_404(Volume, id = vid)
-    return render_to_response('btdst/volume.html',{'volume':volume})
+    return render_to_response('btdst/volume.html',{'volume':volume}, context_instance=RequestContext(request))
 
 def updates(request):
     volumes = Volume.objects.order_by('-id')[:25]
-    return render_to_response('btdst/updates.html',{'volumes':volumes})
+    return render_to_response('btdst/updates.html',{'volumes':volumes}, context_instance=RequestContext(request))
